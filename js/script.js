e93cf52edef7e3369988d75e624624bfa2c00857
liones3000 <liones3000@gmail.com>
2017-08-18 12:15:23 +0300

    start

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 (function($){
 	var server = 'http://helloworld.filonitta.fe4.a-level.com.ua/api/v1';
-	debugger;
+	// debugger;
 	var $studentsList = $('#students-list');
 	var $formInfo = $('#form-info');
 	var $formCreate = $('#form-create');
@@ -14,60 +14,61 @@
 		success: function(response){
 			response.forEach(function(students){
 				$('<a href="">')
-					.addClass('list-group-item')
-					.attr('data-id', students.id)
-					.text(students.name).appendTo($studentsList);
+				.addClass('list-group-item')
+				.attr('data-id', students.id)
+				.text(students.name).appendTo($studentsList);
 			})
 		},
 		error: function(response){}
 	})
 
-$studentsList.on('click', '[data-id]', function(event){
-	event.preventDefault();
-	var studentsId = $(this).data('id');
+	$studentsList.on('click', '[data-id]', function(event){
+		event.preventDefault();
+		var studentsId = $(this).data('id');
 
-	$.ajax({
-		url: server + '/students/' + studentsId,
-		method: 'GET',
-		success: function(response) {
-			for(var key in response){
-				$formInfo.find('[name="' + key + '"]').val(response[key]);
-			}
-		},
-		error: function(error) {},
+		$.ajax({
+			url: server + '/students/' + studentsId,
+			method: 'GET',
+			success: function(response) {
+				for(var key in response){
+					$formInfo.find('[name="' + key + '"]').val(response[key]);
+				}
+			},
+			error: function(error) {},
+		})
 	})
-})
 
 	$btnUpdate.on('click', function(event){
-	var studentsId = $formInfo.find('[name=id]').val();
-	$.ajax({
-		url: server + '/students/' + studentsId,
-		method: 'PUT',
-		data: $formInfo.serialize(),
-		success: function(response){
-			console.log(response);
-			$studentsList
-			.find('[data-id="' + studentsId + '"]')
-			.text(response.lastname + ' ' + response.firstname);
+		debugger
+		var studentsId = $formInfo.find('[name=id]').val();
+		$.ajax({
+			url: server + '/students/' + studentsId,
+			method: 'PUT',
+			data: $formInfo.serialize(),
+			success: function(response){
+				console.log(response);
+				$studentsList
+				.find('[data-id="' + studentsId + '"]')
+				.text(response.lastname + ' ' + response.firstname);
 
-			for(var key in response){
-				$formInfo.find('[name="' + key + '"]').val(response[key]);
-			}
-		},
-		error: function(error){}
-	});
+				for(var key in response){
+					$formInfo.find('[name="' + key + '"]').val(response[key]);
+				}
+			},
+			error: function(error){}
+		});
 	});
 
 	$btnCreate.on('click', function(){
-	event.preventDefault();
+		event.preventDefault();
 
-	console.log(this);
+		console.log(this);
 
-	$.ajax({
-		url: server + '/students/' + studentsId,
-		method: 'POST',
-		success: function(response) {
-			console.log(response);
+		$.ajax({
+			url: server + '/students/' + studentsId,
+			method: 'POST',
+			success: function(response) {
+				console.log(response);
 /*			for(var key in response){
 				$formCreate.find('[name="' + key + '"]').val();
 			}*/
